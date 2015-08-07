45942db8aef39d5f9375d83a89ba26948609dcba
ytake <yuuki.takezawa@comnect.jp.net>
2015-08-07 23:31:28 +0900

    refactor

---
+++ src/Factory/SetterDriver.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Iono\Lom\Factory;
+
+/**
+ * Class GetterDriver
+ *
+ * @package Iono\Lom\Factory
+ * @author  yuuki.takezawa<yuuki.takezawa@comnect.jp.net>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+class SetterDriver extends PropertyReference
+{
+    /**
+     * @return string
+     */
+    protected function resolveMethodName()
+    {
+        return "set" . ucfirst($this->property->getName());
+    }
+}
